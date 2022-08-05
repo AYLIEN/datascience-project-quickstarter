b740edf481cedc3bb207398a39abbe15613208c9
Demian <demian@aylien.com>
2022-08-05 17:07:21 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup
 
+with open("VERSION") as f:
+    version = f.read().strip()
+
 setup(
     name="quickstarter",
-    version=0.1,
+    version=version,
+    description="A tool for quick-starting new datascience projects, used by the research team at AYLIEN.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    readme="README.md",
     packages=["quickstarter"],
     entry_points={
         "console_scripts": [
