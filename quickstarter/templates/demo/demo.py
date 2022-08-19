34d68423d8b9efde0e133b96cb14f6452f4ac1b7
Demian <demian@aylien.com>
2022-08-19 17:16:03 +0100

    fixes

--- quickstarter/templates/demo/demo.py
+++ quickstarter/templates/demo/demo.py
@@ -1,15 +1,17 @@
 import streamlit as st
+# importing tools from your library into the demo:
+from {{PKG_NAME}}.example_module import Counter, TextReverser
 
 
 page_config = st.set_page_config(
-    page_title="Page Title",
+    page_title="Demo",
 )
 
 
 def get_session_state():
     # Initialize session state
     if not st.session_state.get('INIT', False):
-        st.session_state['button_click_count'] = 0
+        st.session_state['counter'] = Counter()
 
     st.session_state['INIT'] = True
     return st.session_state
@@ -17,14 +19,22 @@ def get_session_state():
 
 def main():
     session_state = get_session_state()
+    text_reverser = TextReverser()
+    counter = session_state["counter"]
+
     st.write("# Simple Streamlit Demo")
 
     if st.button("Click Me"):
-        session_state["button_click_count"] += 1
-        clicks = session_state["button_click_count"]
-        st.write(
-            f"This button has been clicked {clicks} times in this session."
-        )
+        counter()
+    clicks = counter.count
+    st.write(
+        f"This button has been clicked {clicks} times in this session."
+    )
+
+    text = st.text_input("Enter text")
+    if st.button("Click to reverse text"):
+        new_text = text_reverser(text)
+        st.write(new_text)
 
 
 if __name__ == '__main__':
